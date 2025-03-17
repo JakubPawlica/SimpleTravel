313c0fbbdae70f2b5bc6f1902e498885f6020eb4
Jakub-PC <quba.pawlica@gmail.com>
2025-03-17 21:45:55 +0100

    fix(api): add missing import in Controllers for handling POST, PUT, DELETE requests

--- apps/back-symfony/src/Controller/TripController.php
+++ apps/back-symfony/src/Controller/TripController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class TripController extends AbstractController
 {
