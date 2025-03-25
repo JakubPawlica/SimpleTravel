1bb8879e9d50861898b5999aa12c6e1d8d1eb376
Jakub-PC <quba.pawlica@gmail.com>
2025-03-25 15:21:11 +0100

    refactor(front): complete Homepage, add responsiveness

--- apps/front-react/src/common/home/Home.jsx
+++ apps/front-react/src/common/home/Home.jsx
@@ -11,12 +11,13 @@ import { login, logout } from "../../features/auth/authSlice";
 import { useSelector } from "react-redux";
 import "./Home.css";
 import Button from "react-bootstrap/Button";
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=globe_asia" />
 
 export default function Home() {
   return (
     <div className="home-container">
       <header className="home-header">
-        <div className="home-logo">SimpleTravel</div>
+        <div className="home-logo"><span className="material-symbols-outlined">travel</span><span className="simple">Simple</span><span className="travel">Travel</span></div>
         <div className="home-auth">
           <NavLink to="/login" className="home-auth-button">
             Zaloguj / Zarejestruj się
@@ -25,18 +26,18 @@ export default function Home() {
       </header>
       <main className="home-main">
         <div className="hero-content">
-          <h1 className="hero-title">Odkryj nowe przygody</h1>
+          <h1 className="hero-title">Zaplanuj nowe przygody</h1>
           <p className="hero-subtitle">
-            Zarządzaj, planuj, pozostań w kontakcie - wszystko w jednym miejscu.
+            <p>Zarządzaj, planuj, bądź na bieżąco z wydatkami - wszystko w jednym miejscu.</p><p>Wyrusz w swoją nową podróż wraz z SimpleTravel.</p>
           </p>
           <NavLink to="/explore" className="hero-cta-button">
-            Rozpocznij podróż
+            Rozpocznij
           </NavLink>
         </div>
         <div className="hero-image-container">
           <img
             src="https://images01.nicepagecdn.com/c461c07a441a5d220e8feb1a/585d277d6df25b5f99f98670/jjjj-min.png"
-            alt="Podróże"
+            alt="SimpleTravel"
             className="hero-image"
           />
         </div>
