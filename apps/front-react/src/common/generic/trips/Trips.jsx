e74206c9215eb099922ddac103978aa7e563bad7
Jakub-PC <quba.pawlica@gmail.com>
2025-03-31 17:57:10 +0200

    feat(back): add refresh button for listing Trips

--- apps/front-react/src/common/generic/trips/Trips.jsx
+++ apps/front-react/src/common/generic/trips/Trips.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTrip } from '../../../context/useTrip';
 import { useAuth } from '../../../context/useAuth';
 import { toast } from 'react-toastify';
@@ -7,12 +7,25 @@ import { TbMoodSad } from "react-icons/tb";
 import { RiDeleteBin5Line } from "react-icons/ri";
 import { TbCalendarTime } from "react-icons/tb";
 import { FaLocationDot } from "react-icons/fa6";
+import { IoRefresh } from "react-icons/io5";
 import './Trips.css';
 
 export default function Trips() {
   const { trips, loading, refreshTrips } = useTrip();
   const { user } = useAuth();
   const navigate = useNavigate();
+
+  const [refreshSuccess, setRefreshSuccess] = useState(false);
+
+  const handleManualRefresh = async () => {
+    try {
+      await refreshTrips();
+      toast.success('Lista podróży została odświeżona!');
+    } catch (err) {
+      console.error('Błąd odświeżania:', err);
+      toast.error('Nie udało się odświeżyć podróży.');
+    }
+  };  
 
   const handleAddTrip = () => {
     navigate("/dashboard/plan-journey");
@@ -49,10 +62,18 @@ export default function Trips() {
         <p className="blank-trip-page">
           <p className="blank-trip-page-icon"><TbMoodSad /></p>
           <p>Nie masz jeszcze żadnych podróży.</p>
-          <button onClick={handleAddTrip} className="black-trip-page-button">Zaplanuj</button>
+          <div class="blank-trip-page-btn-section">
+            <button onClick={handleAddTrip} className="black-trip-page-button">Zaplanuj</button>
+            <button onClick={handleManualRefresh} className="black-trip-page-button">
+              <IoRefresh class="refresh-trips-btn-icon"/><p>Odśwież podróże</p>
+            </button>
+          </div>
         </p>
       ) : (
         <div className="trip-card-grid">
+          <button onClick={handleManualRefresh} className="refresh-trips-btn">
+            <IoRefresh class="refresh-trips-btn-icon"/><p>Odśwież podróże</p>
+          </button>
           {trips.map(trip => (
             <div key={trip.id} className="trip-card" onClick={() => navigate(`/dashboard/trips/${trip.id}`)}>
               <h3 className="trip-card-title">{trip.tripName}</h3>
