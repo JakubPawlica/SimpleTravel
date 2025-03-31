b2fbbabb3f8d9d1f944dd2de9dc69f6f1bae1029
Jakub-PC <quba.pawlica@gmail.com>
2025-03-31 17:09:11 +0200

    refactor(front): new style for Trips page

--- apps/front-react/src/common/generic/trips/TripDetails.jsx
+++ apps/front-react/src/common/generic/trips/TripDetails.jsx
@@ -3,6 +3,10 @@ import { useNavigate } from "react-router-dom";
 import { useParams } from 'react-router-dom';
 import { TbArrowBackUp } from "react-icons/tb";
 import './TripDetails.css';
+import { FaMapLocationDot } from "react-icons/fa6";
+import { LuCalendarClock } from "react-icons/lu";
+import { FaInfoCircle } from "react-icons/fa";
+import { FaCrown } from "react-icons/fa6";
 
 export default function TripDetails() {
   const { id } = useParams();
@@ -12,6 +16,10 @@ export default function TripDetails() {
 
   const handleGoBackToTrips = () => {
     navigate("/dashboard/trips");
+  };
+
+  const handleGoToSchedule = () => {
+    navigate("/dashboard/schedule");
   };
 
   useEffect(() => {
@@ -38,12 +46,20 @@ export default function TripDetails() {
 
   return (
     <div className="trip-details">
-      <h2>{trip.tripName}</h2>
-      <p><strong>Miejsce:</strong> {trip.destination}</p>
-      <p><strong>Termin:</strong> {trip.startDate.slice(0, 10)} – {trip.endDate.slice(0, 10)}</p>
-      <p><strong>Opis:</strong> {trip.description}</p>
-      <p><strong>Autor:</strong> {trip.createdBy?.name}</p>
-      <button onClick={handleGoBackToTrips}><TbArrowBackUp /></button>
+      <div className="trip-details-header"></div>
+      <div className="trip-details-main">
+        <h2 className="trip-details-title">{trip.tripName}</h2>
+        <div className="trip-details-info">
+          <p><strong><FaMapLocationDot className="trip-details-icon"/> Miejsce:</strong> {trip.destination}</p>
+          <p><strong><LuCalendarClock className="trip-details-icon"/> Termin:</strong> {trip.startDate.slice(0, 10)} – {trip.endDate.slice(0, 10)}</p>
+          <p><strong><FaInfoCircle className="trip-details-icon"/> Opis:</strong> {trip.description}</p>
+          <p><strong><FaCrown className="trip-details-icon"/> Organizator:</strong> {trip.createdBy?.name}</p>
+        </div>
+        <div class="trip-details-buttons">
+          <button onClick={handleGoBackToTrips}><TbArrowBackUp /></button>
+          <button onClick={handleGoToSchedule}><LuCalendarClock className="trip-details-button-calendar"/></button>
+        </div>
+      </div>
     </div>
   );
 }
